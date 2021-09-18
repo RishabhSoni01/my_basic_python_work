b68c9834a07fe2ee46174f7eb1a4008d427f23fb
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2021-09-18 20:58:55 +0000

    Modification1

--- object_oriented/learn_oopsyntax.py
+++ object_oriented/learn_oopsyntax.py
@@ -7,8 +7,15 @@ class employee:
         self.email=first+'.'+second+'@gmail.com'
     def fullname(self):
         return '{} {}'.format(self.first,self.second)
+    def raise_apply(self):
+        self.pay=int(self.pay * 1.04)
+
+#class developer(employee):
+#   def.__init__(self,first,second,pay):
 emp1=employee('Rishabh','soni',5000000)
-emp2=employee('UMANG','BHA LLA',6000000)
+emp2=employee('UMANG','BHALLA',6000000)
 
 print(emp1.email)
 print(emp2.email)
+emp1.raise_apply()
+print(emp1.pay)
