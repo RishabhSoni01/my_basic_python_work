0d39e9b760386867d7229955e61b399d0317f52f
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2021-09-19 11:22:40 +0000

    Employee from string

--- object_oriented/learn_oopsyntax.py
+++ object_oriented/learn_oopsyntax.py
@@ -18,6 +18,14 @@ def set_raise_amount(cls,amount):
     cls.raise_amount=amount
 emp1=employee('Rishabh','soni',5000000)
 emp2=employee('UMANG','BHALLA',6000000)
+emp_str_1='john-doe-70000'
+emp_str_2='steve-smith-30000'
+emp_str_3='virat-kohli-50000'
+
+first, last,pay=emp_str_1.split('-')
+new_emp1=employee(first,last,pay)
+print(new_emp1.email)
+print(new_emp1.pay)
 #print(emp1.email)
 #print(emp2.email)
 #emp1.raise_apply()
@@ -26,8 +34,8 @@ emp2=employee('UMANG','BHALLA',6000000)
 #emp1.raise_amount=2
 #emp2.raise_apply()
 #print(employee.__dict__)
-employee.set_raise_amount(3)
-print(employee.raise_amount)
-print(emp1.raise_amount)
-print(emp2.raise_amount)
-print(employee.no_of_employee)
+#employee.set_raise_amount(3)
+#print(employee.raise_amount)
+#print(emp1.raise_amount)
+#print(emp2.raise_amount)
+#print(employee.no_of_employee)
