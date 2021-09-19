47f9b558b08d6d89d4fbe690ec01529a59fca5cf
RISHABHSONI01 <>
2021-09-19 10:27:38 +0000

    Modification3

--- object_oriented/learn_oopsyntax.py
+++ object_oriented/learn_oopsyntax.py
@@ -8,16 +8,19 @@ class employee:
     def fullname(self):
         return '{} {}'.format(self.first,self.second)
     def raise_apply(self):
-        self.pay=int(self.pay * 1.04)
-
+        self.pay=int(self.pay * employee.raise_amount)
 #class developer(employee):
-#   def.__init__(self,first,second,pay):
+#   def __init__(self,first,second,pay):
 emp1=employee('Rishabh','soni',5000000)
 emp2=employee('UMANG','BHALLA',6000000)
 
-print(emp1.email)
-print(emp2.email)
+#print(emp1.email)
+#print(emp2.email)
 emp1.raise_apply()
+print(emp1.raise_amount)
+#employee.raise_amount=1.05
+emp1.raise_amount=2
 emp2.raise_apply()
-print(emp1.pay)
-print(emp2.pay)
+print(employee.__dict__)
+print(emp1.raise_amount)
+print(emp2.raise_amount)
