c6ac7c5d1dfa6aca41344db4fbd2fc24863a3916
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2021-09-19 11:05:10 +0000

    Class methods

--- object_oriented/learn_oopsyntax.py
+++ object_oriented/learn_oopsyntax.py
@@ -1,26 +1,33 @@
 class employee:
+    no_of_employee=0
     raise_amount=1.04
     def __init__(self,first,second,pay):
         self.first=first
         self.second=second
         self.pay=pay
         self.email=first+'.'+second+'@gmail.com'
+        employee.no_of_employee += 1
     def fullname(self):
         return '{} {}'.format(self.first,self.second)
     def raise_apply(self):
         self.pay=int(self.pay * employee.raise_amount)
 #class developer(employee):
 #   def __init__(self,first,second,pay):
+@classmethod
+def set_raise_amount(cls,amount):
+    cls.raise_amount=amount
 emp1=employee('Rishabh','soni',5000000)
 emp2=employee('UMANG','BHALLA',6000000)
-
 #print(emp1.email)
 #print(emp2.email)
-emp1.raise_apply()
-print(emp1.raise_amount)
+#emp1.raise_apply()
+#print(emp1.raise_amount)
 #employee.raise_amount=1.05
-emp1.raise_amount=2
-emp2.raise_apply()
-print(employee.__dict__)
+#emp1.raise_amount=2
+#emp2.raise_apply()
+#print(employee.__dict__)
+employee.set_raise_amount(3)
+print(employee.raise_amount)
 print(emp1.raise_amount)
 print(emp2.raise_amount)
+print(employee.no_of_employee)
